4663ff1e9ce15a1ea5be43626823c4e4002735a7
dav1n9 <davin9909@gmail.com>
2023-06-30 01:47:59 +0900

    수정

---
+++ lib/api/search_api.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:retrofit/retrofit.dart';
+
+//part 'search_api.g.dart';
+
+@RestApi(baseUrl: "3.34.180.104:4000")
+abstract class SearchApi {
+  String base_url = dotenv.get("base_url");
+
+  //factory SearchApi(Dio dio, {String baseUrl}) = _SearchApi;
+
+  @GET("/voice")
+  Future<Task> getTasks(@Query("spamNumber") String spamNumber);
+}
+
+@JsonSerializable()
+class Task {
+  String massage;
+  int numOfCall;
+  Task({required this.massage, required this.numOfCall});
+  //factory Task.fromJson(Map<String, dynamic> json) => _$TaskFromJson(json);
+}
